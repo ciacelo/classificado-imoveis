3b83d82ff1ebee63149714ccedfeda20fb4cd205
ciacelo <marcelo.martins@dce.ufpb.br>
2019-07-27 02:11:01 -0300

    Create new service component

---
+++ src/app/pages/search/search.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+
+export class SearchComponent implements OnInit {
+  validateForm: FormGroup;
+  demoValue = 100;
+  formatterDollar = (value: number) => `R$ ${value}`;
+  parserDollar = (value: string) => value.replace('R$ ', '');
+
+  submitForm(): void {
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+    }
+  }
+
+  genderChange(value: string): void {
+    this.validateForm.get('note');
+  }
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      note: [null, [Validators.required]],
+      gender: [null, [Validators.required]],
+      type: [null, [Validators.required]]
+    });
+  }
+}
