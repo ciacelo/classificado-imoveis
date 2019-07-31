548e713835d1e7d238e11592964a60a12578c8c1
ciacelo <marcelo.martins@dce.ufpb.br>
2019-07-31 19:38:32 -0300

    Linked components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
   { path: 'search-form', loadChildren: () => import('./pages/search-form/search-form.module').then(m => m.SearchFormModule) },
   { path: 'property/:id', loadChildren: () => import('./pages/property-details/property-details.module').then(m => m.PropertyDetailsModule) },
   { path: 'find', loadChildren: () => import('./components/find/find.module').then(m => m.FindModule) },
-  { path: 'home/my-ads', loadChildren: () => import('./components/my-ads/my-ads.module').then(m => m.MyAdsModule) },
+  { path: 'ad-steps', loadChildren: () => import('./pages/ad-steps/ad-steps.module').then(m => m.AdStepsModule) },
   { path: 'user-profile', loadChildren: () => import('./pages/user-profile/user-profile.module').then(m => m.UserRegisterModule) },
   { path: 'user-favorite', loadChildren: () => import('./pages/user-favorite/user-favorite.module').then(m => m.UserFavoriteModule) },
   { path: 'result-page', loadChildren: () => import('./pages/result-page/result-page.module').then(m => m.ResultPageModule) }
